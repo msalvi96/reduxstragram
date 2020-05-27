31e567dca9d4e8e5ec931de901fbef4e1a4d48ef
Mrunal <msalvi96@stevens.edu>
2020-05-26 21:53:58 -0400

    first commit

---
+++ client/components/Comments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    render() {
+        return (
+            <div className="comment">
+                I am comments
+            </div>
+        )
+
+    }
+});
+
+export default Comments;
---
+++ client/components/Single.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments'
+
+const Single = React.createClass({
+    render() {
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const post = this.props.posts[i];
+        return (
+            <div className="single-photo">
+                <Photo index={i} post={post} {...this.props} />
+                <Comments />
+            </div>
+        )
+    }
+});
+
+export default Single;
